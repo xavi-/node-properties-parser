d460b118d2413e6ff2fe8dcb836a4d9947f57843
Xavi <xavi.rmz@gmail.com>
2012-07-06 20:27:23 -0700

    Added assert statements to test file.

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+var assert = require("assert");
 var prop = require("../index.js");
 
 prop.read("./test-cases.properties", function(err, data) {
@@ -6,6 +7,18 @@ prop.read("./test-cases.properties", function(err, data) {
 	console.log("data:");
 	console.dir(data);
 
+	assert.equal(data["lala"], 'ℊ the foo foo lalala;');
+	assert.equal(data["website"], 'http://en.wikipedia.org/');
+	assert.equal(data["language"], 'English');
+	assert.equal(data["message"], 'Welcome to Wikipedia!');
+	assert.equal(data["key with spaces"], 'This is the value that could be looked up with the key "key with spaces".');
+	assert.equal(data["tab"], '\t');
+	assert.equal(data["long-unicode"], '\u00000009');
+	assert.equal(data["space separator"], 'key val \n three');
+	assert.equal(data["another-test"], ':: hihi');
+	assert.equal(data["null-prop"], '');
+	assert.ok(data["valueOf"] == null, "Properties are set that shouldn't be (valueOf)");
+	assert.ok(data["toString"] == null, "Properties are set that shouldn't be (toString)");
 
 	var repl = require("repl").start();
 	repl.context.data = data;
